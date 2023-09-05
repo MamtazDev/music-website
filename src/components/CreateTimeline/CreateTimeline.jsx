44683d33041738ac04df341bffbbca1532e26c19
nipaaa <akternipa941@gmail.com>
2023-09-05 16:59:53 +0600

    update

--- src/components/CreateTimeline/CreateTimeline.jsx
+++ src/components/CreateTimeline/CreateTimeline.jsx
@@ -1,22 +1,26 @@
-import React from 'react';
-import Stepper from '../Stepper/Stepper';
-import './CreateTimeline.css'
+import React from "react";
+import Stepper from "../Stepper/Stepper";
+import "./CreateTimeline.css";
 const CreateTimeline = () => {
-    return (
-        <div >
-           <div className='container'> 
-           <div class="edit_section_container">
-                <div class="edit_container">
-                    <h2 class="edit_heading">Edit Music Timeline</h2>
-                    <label className='label add_item_label' >Add Item</label>
-                        <input style={{ marginTop: "8px" }} className="login_input2 add_item_field w-full  border border-solid  rounded" type="text"  />
-                    <button class="btn edit_button">Add</button>
-                </div>
-            </div>
-           </div>
-            <Stepper />
+  return (
+    <div className="mt-[126px]">
+      <div className="container">
+        <div class="edit_section_container">
+          <div class="edit_container">
+            <h2 class="edit_heading">Edit Music Timeline</h2>
+            <label className="label add_item_label">Add Item</label>
+            <input
+              style={{ marginTop: "8px" }}
+              className="login_input2 add_item_field w-full  border border-solid  rounded"
+              type="text"
+            />
+            <button class="btn edit_button">Add</button>
+          </div>
         </div>
-    );
+      </div>
+      <Stepper />
+    </div>
+  );
 };
 
 export default CreateTimeline;
